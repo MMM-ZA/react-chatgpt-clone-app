cba5d8b278ee49233cd333d87750360f3d8787eb
makoma mabilo <makoma44@gmail.com>
2023-05-11 15:19:31 +0800

    adding to side bar and main section structure, added styling

--- react-chatgpt-clone-app/src/App.js
+++ react-chatgpt-clone-app/src/App.js
@@ -5,12 +5,27 @@ const App = () =>  {
     <div className="app">
       <section className="side-bar">
         <button> + New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+           <li>facts</li>
+        </ul>
         <nav>
           <p>Made by Makoma</p>
         </nav>
       </section>
-      <section className="main-section"></section>
+      <section className="main-section">
+        <h1>AscendGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section ">
+         <div className="input-container">
+           <input/>
+           <div id="submit">➢</div>
+         </div>
+         <p className="info">
+            ChatGPT is a large language model based on the GPT-3.5 architecture, developed by OpenAI. It is designed to provide human-like responses to natural language input and is trained on a massive corpus of text data.  </p>
+        </div>
+      </section>
 
     </div>
   );
