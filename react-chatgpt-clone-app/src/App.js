acbe49f04644af822279380bc30739040ce37510
makoma mabilo <makoma44@gmail.com>
2023-05-15 15:31:23 +0800

    added server.js, managing http requests

--- react-chatgpt-clone-app/src/App.js
+++ react-chatgpt-clone-app/src/App.js
@@ -1,6 +1,26 @@
 
 
 const App = () =>  {
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "hello how are you? "
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+     const response = await fetch ('https://localhost:8000/completions', options)
+     const data =  await response.json()
+     console.log(data)
+    } catch (error) {
+      console.error(error)
+
+    }
+  }
   return (
     <div className="app">
       <section className="side-bar">
@@ -20,7 +40,7 @@ const App = () =>  {
         <div className="bottom-section ">
          <div className="input-container">
            <input/>
-           <div id="submit">➢</div>
+           <div id="submit" onClick={getMessages}>➢</div>
          </div>
          <p className="info">
             ChatGPT is a large language model based on the GPT-3.5 architecture, developed by OpenAI. It is designed to provide human-like responses to natural language input and is trained on a massive corpus of text data.  </p>
